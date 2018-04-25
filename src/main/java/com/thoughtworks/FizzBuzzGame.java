c1ac05e2801d063d54a5d010dbb5403044d6714d
xuexue <1047504737@qq.com>
2018-04-25 19:34:55 +0800

    FizzBuzzWhuzzGame coding complete

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -1,16 +1,123 @@
 package com.thoughtworks;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class FizzBuzzGame {
     private List<String> results = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+    private int[] specialNum = new int[3];
 
-    public void start(int count) {
-
+    public static void main(String[] args){
+        new FizzBuzzGame().run();
+    }
+    public void run(){
+        setThreeSpecialNumber();
+        int count = sc.nextInt();
+        start(count);
+        System.out.println(getResults().toString());
     }
 
+    /**
+     * 体育老师说出3个不同的个位数
+     */
+    private void setThreeSpecialNumber(){
+        Set<Integer> set = new HashSet<>();
+        int i=0;
+        while(i<3){
+            int num = sc.nextInt();
+            if(num>0&&num<10){
+                set.add(num);
+                i = set.size();
+            }
+        }
+        specialNum = set.stream().sorted().mapToInt(Integer::intValue).toArray();
+        return ;
+    }
+
+    /**
+     * 通过数组对体育老师说出的3个不同的数进行赋值
+     * @param arr
+     */
+    public void setThreeSpecialNumber(int[] arr){
+        specialNum = arr;
+    }
+
+    /**
+     * 根据输入的count,构造results
+     * @param count
+     */
+    public void start(int count) {
+        for (int i = 1; i < count; i++) {
+            String result="";
+            if(containsSpecialNum1(i)){
+                result+="Fizz";
+            }else if(isSpecialNum1Times(i)||isSpecialNum2Times(i)||isSpecialNum3Times(i)){
+                if(isSpecialNum1Times(i)){
+                    result+="Fizz";
+                }
+                if(isSpecialNum2Times(i)){
+                    result+="Buzz";
+                }
+                if(isSpecialNum3Times(i)){
+                    result+="Whizz";
+                }
+            }else{
+                result+=i;
+            }
+            results.add(result);
+        }
+    }
+
+    /**
+     * 判断是否包含第一个特殊数
+     * @param num
+     * @return
+     */
+    public boolean containsSpecialNum1(int num){
+        String str = String.valueOf(num);
+        char[] arr = str.toCharArray();
+        for(char c:arr){
+            if(c-'0' == specialNum[0]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 判断是否是第一个特殊数的倍数
+     * @param num
+     * @return
+     */
+    public boolean isSpecialNum1Times(int num){
+        if(num%specialNum[0]==0)
+            return true;
+        return false;
+    }
+
+    /**
+     * 判断是否是第二个特殊数的倍数
+     * @param num
+     * @return
+     */
+    public boolean isSpecialNum2Times(int num){
+        if(num%specialNum[1]==0)
+            return true;
+        return false;
+    }
+
+    /**
+     * 判断是否是第三个特殊数的倍数
+     * @param num
+     * @return
+     */
+    public boolean isSpecialNum3Times(int num){
+        if(num%specialNum[2]==0)
+            return true;
+        return false;
+    }
     public List<String> getResults() {
         return results;
     }
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -1,10 +1,47 @@
 package com.thoughtworks;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class FizzBuzzGameTest {
+    private FizzBuzzGame fbg = new FizzBuzzGame();
+    private List<String> results = new ArrayList<>();
+
+    @Before
+    public void setup(){
+        int[] arr = {3,5,7};
+        fbg.setThreeSpecialNumber(arr);
+        fbg.start(100);
+        results = fbg.getResults();
+    }
     @Test
     public void should_return_Fizz_when_have_3() {
-
+        assertEquals(results.get(2),"Fizz");
+    }
+    @Test
+    public void should_return_Buzz_when_have_5() {
+        assertEquals(results.get(4),"Buzz");
+    }
+    @Test
+    public void should_return_Whizz_when_have_7() {
+        assertEquals(results.get(6),"Whizz");
+    }
+    @Test
+    public void should_return_FizzBuzz_when_have_15() {
+        assertEquals(results.get(14),"FizzBuzz");
+    }
+    @Test
+    public void should_return_FizzWhizz_when_have_21() {
+        assertEquals(results.get(20),"FizzWhizz");
+    }
+    //BuzzWhizz
+    @Test
+    public void should_return_BuzzWhizz_when_have_70() {
+        assertEquals(results.get(69),"BuzzWhizz");
     }
 }
